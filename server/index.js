f3c3a2105afc6fd674425282a8700926caef072c
Vinnie Park <parkvinnie@gmail.com>
2024-08-17 15:12:48 -0700

    python dependencies

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
